867c266a2a7acc14b29e39606244afd243c386bd
anastasijadimovska <anastasija.dimovska@kiimo.me>
2024-12-06 19:13:36 +0100

    Initial project set up

---
+++ MyBusMap/src/main/java/mk/com/finki/mybusmap/MyBusMapApplication.java
@@ -0,0 +1,13 @@
+package mk.com.finki.mybusmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBusMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBusMapApplication.class, args);
+    }
+
+}
---
+++ MyBusMap/src/test/java/mk/com/finki/mybusmap/MyBusMapApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.com.finki.mybusmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBusMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
