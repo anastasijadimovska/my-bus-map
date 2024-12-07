fe3c9a60388ac9ab29beec2d5eb65b3af82ada78
Slobodan Ivanovski <sivanovski317@gmail.com>
2024-12-07 15:49:43 +0100

    Initial Security Implementation

---
+++ MyBusMap/src/main/java/mk/com/finki/mybusmap/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package mk.com.finki.mybusmap.repository;
+
+import mk.com.finki.mybusmap.model.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email); // Use 'email' if that is the correct field for login
+}
