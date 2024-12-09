30b60a75bd3e8087fd8f2d42ee3ff14b1d433f2d
anastasijadimovska <anastasija.dimovska@kiimo.me>
2024-12-09 10:58:54 +0100

    -Bus, busStop, busSchedule,BusLine to be accessed only if the user is logged in -Exception handling for 401 and 403

---
+++ MyBusMap/src/main/java/mk/com/finki/mybusmap/config/AccessDeniedEntryPoint.java
@@ -0,0 +1,21 @@
+
+package mk.com.finki.mybusmap.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AccessDeniedEntryPoint implements AccessDeniedHandler {
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.getWriter().write(accessDeniedException.getMessage());
+    }
+}
